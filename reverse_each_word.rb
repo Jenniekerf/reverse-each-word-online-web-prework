9a2f40cbb3e39564650e07c35e9b6912e95bf00f
Jennie Kerfstedt <jenniekerf@gmail.com>
2018-12-19 20:47:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
   new_sentence = sentence.split
-  new_sentence.collect {|new_sentence| new_sentence.reverse}
+  new_sentence.collect {|new_sentence| new_sentence.reverse!}
 new_sentence.join(" ")
 end
 
